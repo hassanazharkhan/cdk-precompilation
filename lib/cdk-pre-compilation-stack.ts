176ac5a74852b39e1fbe70cc394c0f29ad111d5f
Hassan Azhar Khan <hassanazharkhan@gmail.com>
2021-11-11 19:13:27 +0500

    chore: project created using cdk init

--- lib/cdk-pre-compilation-stack.ts
+++ lib/cdk-pre-compilation-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
-
+import * as lambdaNodejs from '@aws-cdk/aws-lambda-nodejs'
 export class CdkPreCompilationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new lambdaNodejs.NodejsFunction(this, 'PreCompiledLambda', {
+      entry: './lib/lambda/index.ts',
+      bundling: {
+        preCompilation: true
+      }
+    })
   }
 }
---
+++ lib/lambda/index.ts
@@ -0,0 +1,6 @@
+export const handler = (event: any) => {
+    return {
+        statusCode: 200,
+        message: 'Success'
+    }
+}
